28166eeabffdf004204cff63d9361b914d053106
Isaac M Bazian <bazia001@umn.edu>
2023-02-24 09:24:10 -0600

    Completed data import

--- R/week6.R
+++ R/week6.R
@@ -1,3 +1,23 @@
 # Script Settings and Resources
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
+library(rebus)
+library(stringi)
+
+
+# Data Import
+citations <- stri_read_lines("../data/citations.txt")
+citations_txt <- citations[!(stri_isempty(citations))]
+length(citations) - length(citations_txt)
+mean(str_length(citations_txt))
+
+# Data Cleaning
+
+
+
+
+
+
+
+
+
