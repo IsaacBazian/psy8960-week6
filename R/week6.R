7cee203b3972f08c5be9ebdef7847ce260b7f8a1
Isaac M Bazian <bazia001@umn.edu>
2023-02-24 10:08:06 -0600

    Removed single and double quotes

--- R/week6.R
+++ R/week6.R
@@ -12,9 +12,10 @@ length(citations) - length(citations_txt)
 mean(str_length(citations_txt))
 
 # Data Cleaning
+sample(citations_txt, 10)
 
-
-
+citations_tbl <- tibble(line = 1:length(citations_txt), cite = citations_txt) %>%
+  mutate(cite = str_replace_all(string = cite, pattern = regex("[\"\']"), replacement = ""))
 
 
 
