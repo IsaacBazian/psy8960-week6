4827e18f14cff5b6c7bc95d160b5b4e53e2a557d
Isaac M Bazian <bazia001@umn.edu>
2023-02-24 14:18:32 -0600

    Added count of how many first_author cells aren't NAs. Took a look, I am grabbing some non-names with my code, will reassess how much the optional comma is contributing to that.

--- R/week6.R
+++ R/week6.R
@@ -22,8 +22,4 @@ citations_tbl <- tibble(line = 1:length(citations_txt), cite = citations_txt) %>
   mutate(title = str_match(string = cite, pattern = "\\)\\.\\s([^\\.]+[.?!])")[,2]) %>% 
   mutate(first_author = str_match(string = cite, pattern = optional(STAR) %R% capture(one_or_more(WRD) %R% optional(",") %R% SPC %R% WRD %R% DOT %R% optional(SPC) %R% optional(WRD) %R% optional(DOT)))[,2])
 
-
-
-
-
-
+sum(!is.na(citations_tbl$first_author))
