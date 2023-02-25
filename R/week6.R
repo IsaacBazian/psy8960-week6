9d9fa926f06b9d3f32a673e86369450d889ed9aa
Isaac M Bazian <bazia001@umn.edu>
2023-02-25 10:28:25 -0600

    Removed the stringr::regex function from most lines because it was unnecessary, retained it on line 21 so I could easily ignore case sensitivity but still dropped the stringr:: notation because it is no longer masked by having rebus called. I have elected to leave the comma as optional after capturing the first author's surname, because now that I've added the specification that this regex is at the start of each string I am more confident that I am mostly capturing APA 7 style names, even if they have dropped the comma that APA 7 technically requires.

--- R/week6.R
+++ R/week6.R
@@ -15,10 +15,10 @@ mean(str_length(citations_txt))
 sample(citations_txt, 10)
 
 citations_tbl <- tibble(line = 1:length(citations_txt), cite = citations_txt) %>%
-  mutate(cite = str_replace_all(string = cite, pattern = stringr::regex("[\"\']"), replacement = "")) %>%
-  mutate(year = str_match(string = cite, pattern =  stringr::regex("\\(([\\d]+)\\)"))[,2]) %>%
-  mutate(page_start = str_match(string = cite, pattern = stringr::regex("([\\d]+)-"))[,2]) %>% 
-  mutate(perf_ref = str_detect(string = cite, pattern = stringr::regex("performance", ignore_case = T))) %>% 
+  mutate(cite = str_replace_all(string = cite, pattern = "[\"\']", replacement = "")) %>%
+  mutate(year = str_match(string = cite, pattern =  "\\(([\\d]+)\\)")[,2]) %>%
+  mutate(page_start = str_match(string = cite, pattern = "([\\d]+)-")[,2]) %>% 
+  mutate(perf_ref = str_detect(string = cite, pattern = regex("performance", ignore_case = T))) %>% 
   mutate(title = str_match(string = cite, pattern = "\\)\\.\\s([^\\.]+[.?!])")[,2]) %>% 
   mutate(first_author = str_match(string = cite, pattern = "^[\\*]?([\\w]+[,]?\\s\\w\\.[\\s]?[\\w]?[\\.]?)")[,2])
 
