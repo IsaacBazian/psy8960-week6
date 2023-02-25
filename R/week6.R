c769a614ee4bee002fccf4dd9584e39915c5287f
Isaac M Bazian <bazia001@umn.edu>
2023-02-25 10:00:10 -0600

    Changed page_start variable to regex from rebus

--- R/week6.R
+++ R/week6.R
@@ -17,7 +17,7 @@ sample(citations_txt, 10)
 citations_tbl <- tibble(line = 1:length(citations_txt), cite = citations_txt) %>%
   mutate(cite = str_replace_all(string = cite, pattern = stringr::regex("[\"\']"), replacement = "")) %>%
   mutate(year = str_match(string = cite, pattern =  stringr::regex("\\(([\\d]+)\\)"))[,2]) %>%
-  mutate(page_start = str_match(string = cite, pattern = capture(one_or_more(DGT)) %R% "-")[,2]) %>% 
+  mutate(page_start = str_match(string = cite, pattern = stringr::regex("([\\d]+)-"))[,2]) %>% 
   mutate(perf_ref = str_detect(string = cite, pattern = stringr::regex("performance", ignore_case = T))) %>% 
   mutate(title = str_match(string = cite, pattern = "\\)\\.\\s([^\\.]+[.?!])")[,2]) %>% 
   mutate(first_author = str_match(string = cite, pattern = optional(STAR) %R% capture(one_or_more(WRD) %R% optional(",") %R% SPC %R% WRD %R% DOT %R% optional(SPC) %R% optional(WRD) %R% optional(DOT)))[,2])
