acec5924288df9eeb56e8ee19503f371acdaa98b
Isaac M Bazian <bazia001@umn.edu>
2023-02-25 10:11:50 -0600

    Changed first_author variable to regex from rebus. Removed rebus library call as it should no longer be necessary.

--- R/week6.R
+++ R/week6.R
@@ -1,8 +1,8 @@
 # Script Settings and Resources
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
-library(rebus)
 library(stringi)
+
 
 
 # Data Import
@@ -20,6 +20,6 @@ citations_tbl <- tibble(line = 1:length(citations_txt), cite = citations_txt) %>
   mutate(page_start = str_match(string = cite, pattern = stringr::regex("([\\d]+)-"))[,2]) %>% 
   mutate(perf_ref = str_detect(string = cite, pattern = stringr::regex("performance", ignore_case = T))) %>% 
   mutate(title = str_match(string = cite, pattern = "\\)\\.\\s([^\\.]+[.?!])")[,2]) %>% 
-  mutate(first_author = str_match(string = cite, pattern = optional(STAR) %R% capture(one_or_more(WRD) %R% optional(",") %R% SPC %R% WRD %R% DOT %R% optional(SPC) %R% optional(WRD) %R% optional(DOT)))[,2])
+  mutate(first_author = str_match(string = cite, pattern = "^[\\*]?([\\w]+[,]?\\s\\w\\.[\\s]?[\\w]?[\\.]?)")[,2])
 
 sum(!is.na(citations_tbl$first_author))
